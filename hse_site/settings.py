e22b3b906adfbc7be05b4aa1f79978b9b1268681
frankwrk <francisc.frd@gmail.com>
2020-04-22 15:14:09 -0400

    Fix settings syntax

--- hse_site/settings.py
+++ hse_site/settings.py
@@ -98,8 +98,9 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 DATABASES = {
     "default": dj_database_url.config(default=config("DATABASE_URL", conn_max_age=500, ssl_require=True)
-    }
+    ),
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
